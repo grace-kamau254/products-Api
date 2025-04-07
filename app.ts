dd7204a83718e46a301c5de816cc8c0418d75d52
grace-kamau254 <kamaugraciie@gmail.com>
2025-04-07 15:02:18 +0300

    fixed a bug

--- app.ts
+++ app.ts
@@ -6,7 +6,11 @@ const app = express();
 app.use(express.json());
 app.use('/products', productRoutes);
 
-
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+  });
+  
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
